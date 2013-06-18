3d8e2eb78edb6fa59e824aea6222995b5631b0ea
Kevin Davies <kdavies4@gmail.com>
2013-06-18 16:06:06 -0400

    Empty release branch

--- Resources/Scripts/load-in-unix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Open the FCSys package in Dymola on a Unix/Linux platform.
-
-# Change to the FCSys base directory.
-baseDir=`dirname $0`
-cd $baseDir/../../
-
-# Start Dymola and run a Modelica script to initialize the package.
-dymola ./load.mos &
